8dcd5d8df96a850caedbf748d6b21a3b359b2cbe
James Romero <comsci.romeroj@gmail.com>
2020-12-06 18:13:07 -0800

    Added Main Menu and Button Functionality

---
+++ Cash In/Cash In/MainMenu.swift
@@ -0,0 +1,51 @@
+//
+//  MainMenu.swift
+//  Cash In
+//
+//  Created by James Romero on 12/6/20.
+//
+import SpriteKit
+
+class MainMenu: SKScene {
+
+    func loadGame() {
+        /* 1) Grab reference to our SpriteKit view */
+        guard let skView = self.view as SKView? else {
+            print("Could not get Skview")
+            return
+        }
+
+        /* 2) Load Game scene */
+        guard let scene = GameScene(fileNamed:"GameScene") else {
+            print("Could not make GameScene, check the name is spelled correctly")
+            return
+        }
+
+        /* 3) Ensure correct aspect mode */
+        scene.scaleMode = .aspectFill
+
+        /* Show debug */
+        skView.showsPhysics = true
+        skView.showsDrawCount = true
+        skView.showsFPS = true
+
+        /* 4) Start game scene */
+        skView.presentScene(scene)
+    }
+    
+
+/* UI Connections */
+    var startButton: MSButtonNode!
+
+ override func didMove(to view: SKView) {
+    /* Setup your scene here */
+    
+    /* Set UI connections */
+    startButton = self.childNode(withName: "startButton") as! MSButtonNode
+    startButton.selectedHandler = {
+        self.loadGame()
+    }
+
+    }
+ 
+}
