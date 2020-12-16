ee834a41f581c119f1542d96034d21a109c377ca
CodedreamerJ <comsci.romeroj@gmail.com>
2020-12-15 21:46:18 -0800

    Game score and Gameplay added

--- Cash In/Cash In/GameViewController.swift
+++ Cash In/Cash In/GameViewController.swift
@@ -35,12 +35,14 @@ class GameViewController: UIViewController {
     //This button will be used to allow user to take his money and dip(another word for go)
     @IBOutlet weak var contentButton: UIButton!
     //This label will be used as a score feature which will display money earned
-    @IBOutlet weak var gameScore: UITextField!
+    @IBOutlet weak var gameScore: UILabel!
     
     //Array of question structures that was created
    var Questions = [ListofQuestions]() //Type Inference
    var questionNumber = Int()
    var answerNumber = Int()
+   var score = Int()
+   var currentQuestion = Int()
     
 
 
@@ -67,16 +69,16 @@ class GameViewController: UIViewController {
             //We will add our questions manually. There is an easier way of doing this as reading in questions from a file. Still learing the swift language
             Questions = [ListofQuestions(Question: "In the UK, the abbreviation NHS stands for National what Service?", Answers: ["Humanity", "Health", "Honour", "Household"], Answer: 2),
                          ListofQuestions(Question: "Which Disney character famously leaves a glass slipper behind at a royal ball?", Answers: ["Pocahontas", "Sleeping Beauty", "Cinderella", "Elsa"], Answer: 3),
-                         ListofQuestions(Question: "What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?", Answers: ["Hangar", "Terminal", "Concourse", "Carousel"], Answer: 4),
+                         ListofQuestions(Question: "What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?", Answers: ["Hangar", "Terminal", "Concourse", "Carousel"], Answer: 3),
                          ListofQuestions(Question: "Which of these brands was chiefly associated with the manufacture of household locks?", Answers: ["Phillips", "Flymo", "Chubb", "Ronseal"], Answer: 3),
                          ListofQuestions(Question: "The hammer and sickle is one of the most recognisable symbols of which political ideology?", Answers: ["Republicanism", "Communism", "Conservatism", "Liberalism"], Answer: 2),
-                         ListofQuestions(Question: "Which toys have been marketed with the phrase “robots in disguise”?", Answers: ["Bratz Dolls", "Sylvanian Families", "Hatchimals", "Transformers"], Answer: 4),
+                         ListofQuestions(Question: "Which toys have been marketed with the phrase “robots in disguise”?", Answers: ["Bratz Dolls", "Sylvanian Families", "Hatchimals", "Transformers"], Answer: 3),
                          ListofQuestions(Question: "What does the word loquacious mean?”?", Answers: ["Angry", "Chatty", "Beautiful", "Shy"], Answer: 2),
                          ListofQuestions(Question: "Obstetrics is a branch of medicine particularly concerned with what?", Answers: ["Childbirth", "Broken bones", "Heart conditions", "Old age"], Answer: 1),
                          ListofQuestions(Question: "In Doctor Who, what was the signature look of the fourth Doctor, as portrayed by Tom Baker?", Answers: ["Bow-tie, braces and tweed jacket", "Wide-brimmed hat and extra long scarf", "Pinstripe suit and trainers", "Cape, velvet jacket and frilly shirt"], Answer: 2),
-                         ListofQuestions(Question: "Which of these religious observances lasts for the shortest period of time during the calendar year?", Answers: ["Bahamas", "US Virgin Islands", "Turks and Caicos Islands", "Bermuda"], Answer: 1),
-                         ListofQuestions(Question: "At the closest point, which island group is only 50 miles south-east of the coast of Florida?", Answers: ["Angry", "Chatty", "Beautiful", "Shy"], Answer: 2),
-                         ListofQuestions(Question: "Construction of which of these famous landmarks was completed first?", Answers: ["Empire State Building", "Royal Albert Hall", "Eiffel Tower", "Big Ben Clock Tower"], Answer: 4)]
+                         ListofQuestions(Question: "Which of these religious observances lasts for the shortest period of time during the calendar year?", Answers: ["Ramadan", "Diwali", "Lent", "Hanukkah"], Answer: 1),
+                         ListofQuestions(Question: "At the closest point, which island group is only 50 miles south-east of the coast of Florida?", Answers: ["Bahamas", "US Virgin Islands", "Turks and Caicos Islands", "Bermuda"], Answer: 2),
+                         ListofQuestions(Question: "Construction of which of these famous landmarks was completed first?", Answers: ["Empire State Building", "Royal Albert Hall", "Eiffel Tower", "Big Ben Clock Tower"], Answer: 3)]
                          
             
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,13 +86,17 @@ class GameViewController: UIViewController {
     
     //Function is used to select a question but at the same time remove it so that the same question is not used again within the same game
     func PickQuestion(){
-        
-        if Questions.count > 0{
+    
+        if Questions.count > 1{
+            var x: Int = 0
             //Random question generator
             questionNumber = Int(arc4random()) % Questions.count
             qLabel.text = Questions[questionNumber].Question //adds one of the question to the label
             //We are using a number index to place the question in one of the for buttons
-            answerNumber = Questions[questionNumber].Answer
+            x = currentQuestion
+            Score(currentQuestion: &x)
+            
+           
             
             for i in 0..<Buttons.count{
                 //This for loop is used to place 4 of the potential answers inside the buttons
@@ -103,12 +109,61 @@ class GameViewController: UIViewController {
         else{
             NSLog("Done")
         }
+        currentQuestion += 1
     }
+   
+    
+    func Score(currentQuestion: inout Int){
+        
+        if currentQuestion == 1{
+         score = 4000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 2{
+         score = 8000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 3{
+         score = 16000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 4{
+         score = 32000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 4{
+         score = 62000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 5{
+         score = 124000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 6{
+         score = 248000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 7{
+         score = 496000
+         gameScore.text = "$$ \(score)"
+        }
+        if currentQuestion == 8{
+         score = 10000000
+         gameScore.text = "$$ \(score)"
+        }
+        
+        else {
+            NSLog("Wrong Answer!")
+        }
+    }
+    
+    
     
     @IBAction func Btn1(_ sender: Any) {
         if answerNumber == 0{
             
             PickQuestion()
+        
         }
         else{
             NSLog("Wrong Answer!") //We can maybe add you won this anount of cash in that statement.
@@ -123,6 +178,7 @@ class GameViewController: UIViewController {
         if answerNumber == 1{
             
             PickQuestion()
+         
         }
         else{
             NSLog("Wrong Answer!") //We can maybe add you won this anount of cash in that statement.
@@ -137,6 +193,7 @@ class GameViewController: UIViewController {
         if answerNumber == 2{
             
             PickQuestion()
+            
         }
         else{
             NSLog("Wrong Answer!") //We can maybe add you won this anount of cash in that statement.
@@ -151,6 +208,7 @@ class GameViewController: UIViewController {
         if answerNumber == 3{
             
             PickQuestion()
+            
         }
         else{
             NSLog("Wrong Answer!") //We can maybe add you won this anount of cash in that statement.
