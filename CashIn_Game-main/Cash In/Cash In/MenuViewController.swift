c3404fd5eb4a327b11719d0bf542aa6f5275462c
willard1371 <60620692+willard1371@users.noreply.github.com>
2020-12-16 05:02:41 -0800

    Updated quit function and scoring
    Quit function and scoring function working, requires further refinement.

---
+++ CashIn_Game-main/Cash In/Cash In/MenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MenuViewController.swift
+//  Cash In
+//
+//  Created by James Romero & Dustin Willard on 12/11/20.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class MenuViewController: UIViewController {
+    @IBAction func playButtonPressed(_ sender: AnyObject) {
+        self.performSegue(withIdentifier: "GameViewSegue", sender: self)   }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    
+        
+        }
+     }
+ 
